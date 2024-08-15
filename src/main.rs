e00e74a90036fe28f680e902038dfa8193761d10
Daniel Giao <daniel.giao@icloud.com>
2024-08-15 00:31:33 -0400

    display weather info and temperature emoji

--- src/main.rs
+++ src/main.rs
@@ -41,3 +41,54 @@ fn get_weather_info(
     let response_json = response.json::<WeatherResponse>()?;
     Ok(response_json)
 }
+
+fn display_weather_info(response: &WeatherResponse) {
+    let description = &response.weather[0].description;
+    let temperature = response.main.temp;
+    let humidity = response.main.humidity;
+    let pressure = response.main.pressure;
+    let wind_speed = response.wind.speed;
+
+    let weather_text = format!(
+        "Weather in {}: {} {}
+        > Temperature: {:.1}°C, 
+        > Humidity: {:.1}%, 
+        > Pressure: {:.1} hPa, 
+        > Wind Speed: {:.1} m/s",
+        response.name,
+        description,
+        get_temperature_emoji(temperature),
+        temperature,
+        humidity,
+        pressure,
+        wind_speed,
+    );
+
+    let weather_text_colored = match description.as_str() {
+        "clear sky" => weather_text.bright_yellow(),
+        "few clouds" | "scattered clouds" | "broken clouds" => weather_text.bright_blue(),
+        "overcast clouds" | "mist" | "haze" | "smoke" | "sand" | "dust" | "fog" | "squalls" => {
+            weather_text.dimmed()
+        }
+        "shower rain" | "rain" | "thunderstorm" | "snow" => weather_text.bright_cyan(),
+        _ => weather_text.normal(),
+    };
+
+    println!("{}", weather_text_colored);
+}
+
+fn get_temperature_emoji(temperature: f64) -> &'static str {
+    if temperature < 0.0 {
+        "❄️"
+    } else if temperature >= 0.0 && temperature < 10.0 {
+        "☁️"
+    } else if temperature >= 10.0 && temperature < 20.0 {
+        "⛅"
+    } else if temperature >= 20.0 && temperature < 30.0 {
+        "🌤️"
+    } else {
+        "🔥"
+    }
+}
+
+
